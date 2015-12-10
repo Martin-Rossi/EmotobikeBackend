6660aa6004e2272bc4502b259cd3076ed3d7206f
Kocka Jimmy <kockajimmy@gmail.com>
2015-12-10 20:14:16 +0100

    Add unit test for updating comments

--- tests/CommentTest.php
+++ tests/CommentTest.php
@@ -19,4 +19,19 @@ class CommentTest extends TestCase {
              ->assertEquals( 200, $response->status() );
     }
 
+    public function testUpdateObject() {
+        $comment = factory( App\Comment::class, 1 )->create();
+        $data = factory( App\Comment::class, 1 )->make()->toArray();
+
+        $user = factory( App\User::class )->create();
+
+        $comment->author = $user->id;
+        $comment->save();
+
+        $response = $this->actingAs( $user )->call( 'PUT', '/comments/' . $comment->id, $data );
+
+        $this->seeInDatabase( 'comments', ['text' => $data['text']] )
+             ->assertEquals( 200, $response->status() );
+    }
+
 }
