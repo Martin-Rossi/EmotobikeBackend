de97475a3bc671ee095bd02915e2ee5a4115aa4f
Kocka Jimmy <kockajimmy@gmail.com>
2016-01-05 12:05:00 +0100

    Extend feedback properties

--- app/Feedback.php
+++ app/Feedback.php
@@ -11,6 +11,23 @@ class Feedback extends Model {
     protected $fillable = [
         'foreign_id',
         'foreign_type',
+        'product_id',
+        'offer_id',
+        'shopper_id',
+        'activity_id',
+        'interface_id',
+        'event',
+        'channel',
+        'channel_id',
+        'date',
+        'time',
+        'taxonomy',
+        'behavior',
+        'behavior_frequency',
+        'artifact_id',
+        'artifact_frequency',
+        'interaction_id',
+        'interaction_frequency',
         'value',
         'author'
     ];
